d6aea4ba2a7224747a741c4b7f471452def59234
Ryan Johnson <ryan@mainframe.co>
2016-05-29 16:22:55 +0100

    Fixup aliases
    - Move aliases to separate directories and .htaccess files for speed
    - Add robots.txt and htaccess

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,11 +1,21 @@
+#! /usr/bin/env python
 import os
 import subprocess
 
 subprocess.check_call(["hugo"])
 
-path = os.path.join(os.getcwd(), "public")
-for root, dirs, files in os.walk(path):
-    if root == os.getcwd():
+base_path = os.path.join(os.getcwd(), "public")
+site_path = os.path.join(base_path, "iambismark.net")
+
+aliases = ["blog.iambismark.net", "www.iambismark.net", "iambismark.nfshost.com"]
+
+for alias in aliases:
+    alias_path = os.path.join(site_path, alias)
+    new_path = os.path.join(base_path, alias)
+    os.rename(alias_path, new_path)
+
+for root, dirs, files in os.walk(site_path):
+    if root == site_path:
         continue
     if "index.xml" in files:
         os.remove(os.path.join(root, "index.xml"))
