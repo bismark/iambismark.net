544632b65c5866a94fdddccd0fffd04b3e204df8
Ryan Johnson <ryan@iambismark.net>
2016-08-07 23:33:34 +0100

    regular apn posts

---
+++ scripts/import_regular_apn_posts.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+import codecs
+import json
+from dateutil import parser
+from hugo_helper import utils
+import calendar
+
+import os
+import sys
+import yaml
+
+output_dir = os.path.join(os.getcwd(), "iambismark.net", "content", "post")
+
+
+with open('regular.json', 'r') as f:
+    tweets = json.load(f)
+    for tweet in tweets:
+
+        metadata = {}
+        ts = parser.parse(tweet['created_at'])
+        metadata['date'] = ts.isoformat()
+        metadata['slug'] = str(calendar.timegm(ts.timetuple()))
+        metadata['archive'] = [ts.strftime("%Y-%m")]
+        metadata['alturls'] = ["https://alpha.app.net/bismark/post/{}".format(tweet['id'])]
+
+        year = ts.strftime("%Y")
+        month = ts.strftime("%m")
+
+        output_path = os.path.join(output_dir, year, month)
+        if not os.path.isdir(output_path):
+            os.makedirs(output_path)
+
+        output_filename = os.path.join(output_path, metadata['slug']+".md")
+        if os.path.isfile(output_filename):
+            print "filename conflict", output_filename
+            sys.exit(1)
+
+        with codecs.open(output_filename, 'w', encoding='utf-8') as output_file:
+            output_file.write("---\n")
+            yaml.safe_dump(metadata, output_file, encoding='utf-8', default_flow_style=False, indent=4)
+            output_file.write("---\n\n")
+            output_file.write(tweet['text'])
+            output_file.write("\n")
+
