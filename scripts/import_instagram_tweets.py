29ea55e00a72291dd6afa0a7b0d8cbc75a9c4f01
Ryan Johnson <ryan@iambismark.net>
2016-08-19 23:08:47 +0100

    import instagram posts

---
+++ scripts/import_instagram_tweets.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+
+import codecs
+import json
+from datetime import datetime
+from hugo_helper import utils
+import calendar
+
+import os
+import sys
+import yaml
+
+output_dir = os.path.join(os.getcwd(), "iambismark.net", "content", "post")
+image_dir = os.path.expanduser("~/Desktop/Instagram")
+
+with open('instagram_posts.json', 'r') as f:
+    posts = json.load(f)
+    for post in posts:
+        text = post['text']
+
+        metadata = {}
+        ts = datetime.utcfromtimestamp(post['timestamp'])
+        metadata['date'] = ts.isoformat()
+        metadata['slug'] = str(calendar.timegm(ts.timetuple()))
+        metadata['archive'] = [ts.strftime("%Y-%m")]
+        metadata['alturls'] = [
+            "https://www.instagram.com/p/{}".format(post['code']),
+        ]
+        if 'tweet_id' in post:
+            metadata['alturls'].append(
+                "https://twitter.com/bismark/status/{}".format(post['tweet_id'])
+            )
+        metadata['type'] = 'photo'
+
+        year = ts.strftime("%Y")
+        month = ts.strftime("%m")
+
+        output_path = os.path.join(output_dir, year, month)
+        if not os.path.isdir(output_path):
+            os.makedirs(output_path)
+
+        output_filename = os.path.join(output_path, metadata['slug']+".md")
+        if os.path.isfile(output_filename):
+            print "filename conflict", output_filename
+            sys.exit(1)
+
+        media_file = os.path.join(image_dir, post['file'])
+        ext, sizes = utils.process_image(metadata['slug'], media_file)
+        if ext:
+            metadata['imagetype'] = ext
+        if sizes:
+            metadata['imagesizes'] = sizes
+
+        with codecs.open(output_filename, 'w', encoding='utf-8') as output_file:
+            output_file.write("---\n")
+            yaml.safe_dump(metadata, output_file, encoding='utf-8', default_flow_style=False, indent=4)
+            output_file.write("---\n\n")
+
+            output_file.write(text)
+            output_file.write("\n\n")
+
+#        os.rename(media_file, os.path.join(processed_dir, tweet['filename']))
+
